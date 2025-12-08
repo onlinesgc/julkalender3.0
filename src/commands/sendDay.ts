9c932c705559e2fabfe97eeadedf217ebc89e5ab
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-12-08 15:53:14 +0100

    added a way to send without pinging

--- src/commands/sendDay.ts
+++ src/commands/sendDay.ts
@@ -13,10 +13,12 @@ export class SendDayCommand implements Command {
         .setDescription(this.description)
         .setDefaultMemberPermissions(0)
         .addIntegerOption(option => option.setName("day").setDescription("The day number (1-24)").setRequired(true))
-        .addChannelOption(option => option.setName("channel").setDescription("The channel to send the day content to").setRequired(true));
+        .addChannelOption(option => option.setName("channel").setDescription("The channel to send the day content to").setRequired(true))
+        .addBooleanOption(option => option.setName("ping").setDescription("Whether to ping the advent role").setRequired(false));
     async execute(interaction: ChatInputCommandInteraction) {
         const channel = interaction.options.getChannel("channel", true) as GuildTextBasedChannel;
         const dayNum = interaction.options.getInteger("day", true);
+        const ping = interaction.options.getBoolean("ping") ?? true;
         
         const day = await getDay(dayNum);
         if(!day){
@@ -27,7 +29,7 @@ export class SendDayCommand implements Command {
         await interaction.editReply({ content: `Day ${dayNum} sent to ${channel.toString()}.` });
     }
 
-    async sendDay(channel: GuildTextBasedChannel, dayNum: number, clientId: string) {
+    async sendDay(channel: GuildTextBasedChannel, dayNum: number, clientId: string, ping: boolean = true) {
         const row = new ActionRowBuilder<ButtonBuilder>()
             .addComponents(
                 new ButtonBuilder()
@@ -36,7 +38,7 @@ export class SendDayCommand implements Command {
                     .setLabel(`Lucka ${dayNum}`),
             );
         const clientData = await getClientById(clientId);
-
-        await channel.send({ content: clientData?.adventPingRole ? `<@&${clientData.adventPingRole}>` : "", components: [row] });
+        if (ping) await channel.send({ content: clientData?.adventPingRole ? `<@&${clientData.adventPingRole}>` : "", components: [row] });
+        else await channel.send({ components: [row] });
     }
 }
